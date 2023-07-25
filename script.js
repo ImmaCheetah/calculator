69e9151f29700f14bc48447c3983f04a7fe32cd2
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-07-25 10:14:47 -0400

    Declare variables for numbers and operator

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+let firstNum = 0;
+let secondNum = 0;
+let operator = '';
+
 function add(...nums) {
     const totalAdd = nums.reduce((total, num) => {
         return total + num 
     }, 0)
     return totalAdd;
 }
-
-console.log(add(1, 22, 343));
 
 function subtract(...nums) {
     const totalSub = nums.reduce((total, num) => {
@@ -14,8 +16,6 @@ function subtract(...nums) {
     return totalSub;
 }
 
-console.log(subtract(2, 8));
-
 function divide(...nums) {
     const totalDiv = nums.reduce((total, num) => {
         return total / num 
@@ -23,13 +23,9 @@ function divide(...nums) {
     return totalDiv;
 }
 
-console.log(divide(10, 5, 2));
-
 function multiply(...nums) {
     const totalMult = nums.reduce((total, num) => {
         return total * num 
     })
     return totalMult;
 }
-
-console.log(multiply(10, 5, 2));
