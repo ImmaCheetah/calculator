105dcc821cf7ebb337c86dfa8ebd714367fc8b79
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-07-26 13:59:45 -0400

    Add event listener for operators and function for divide to store numbers

--- script.js
+++ script.js
@@ -41,14 +41,39 @@ function operate(operator, firstNum, secondNum) {
 }
 
 const buttons = document.querySelectorAll('.number-btn');
+const operators = document.querySelectorAll('.operator-btn');
 const result = document.querySelector('#result');
+const division = document.getElementById('divide');
+let displayValue = '';
 
 // Add event listener to all number buttons to show content
 buttons.forEach(button => {
     button.addEventListener('click', function() {
     result.textContent += button.textContent;
-    let displayValue = result.textContent;
-    console.log(result.textContent);
+    displayValue = result.textContent;
+    console.log(displayValue);
     }) 
 });
+
+operators.forEach(operator => {
+    operator.addEventListener('click', function() {
+    result.textContent += operator.textContent;
+    displayValue = result.textContent;
+    // console.log(displayValue);
+    }) 
+});
+
+division.addEventListener('click', doDivide);
+
+function doDivide() {
+    if (displayValue != '' && !displayValue.includes('/')) {
+        firstNum = displayValue;
+        console.log('this is firstnum '+firstNum);
+    } else if (displayValue != '' && displayValue.includes('/')) {
+        secondNum = displayValue;
+        console.log('this is secondnum '+secondNum);
+    } else {
+        console.log('stop');
+    }
+}
 
