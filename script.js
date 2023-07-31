d1992603665e713a74dd6d3da9f56f41f8af2245
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-07-31 10:28:14 -0400

    Refactor code with new update display functionality
    - Create function to display final string separately
    - Change operator event listener to work with updateDisplay()
    - Change numbers event listener to work with updateDisplay()
    - Change variable names for better fit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let firstNum = '';
-let secondNum = '';
-let operator = '';
+let num1 = '';
+let num2 = '';
+let operatorDisplay = '';
 
 function add(...nums) {
     const totalAdd = nums.reduce((total, num) => {
@@ -42,63 +42,43 @@ function operate(operator, firstNum, secondNum) {
 
 const numbers = document.querySelectorAll('.number-btn');
 const operators = document.querySelectorAll('.operator-btn');
-const btnsAndOprts = document.querySelectorAll('.number-btn, .operator-btn');
-const result = document.querySelector('#result');
+
+let display = document.querySelector('#result');
 const division = document.getElementById('divide');
 let displayOperator = '';
 let displayNumber = '';
-let clicked = false;
 
 
-// btnsAndOprts.forEach(button => {
-//     button.addEventListener('click', e => {
-//         if (e.target.innerText === numbers.innerText) {
-//             console.log('yeee');
-//         }
-//     })
-// })
+function updateDisplay() {
+    let displayValue = num1 + operatorDisplay + num2;
+    display.textContent = displayValue;
+    console.log(displayValue);
+}
+
 
 // Add event listener to all number buttons to show content
 numbers.forEach(number => {
     number.addEventListener('click', function() {
-    result.textContent += number.textContent;
-    displayNumber = result.textContent;
-    // console.log(displayNumber);
+        num1 += number.textContent;
+        updateDisplay();
+        if (display.textContent.includes(num1, '/*-+')) {
+            num2 += number.textContent;
+        }
     }) 
 });
 
 operators.forEach(operator => {
     operator.addEventListener('click', e => {
-        if (e.target.innerText === '/') {
-            result.textContent += operator.textContent;
-            displayOperator === '/';
-            firstNum = displayNumber;
-            console.log(displayNumber);
-            console.log('it work');
-        }
+        operatorDisplay += operator.textContent;
+        console.log(operatorDisplay);
+        updateDisplay();
 
-    // result.textContent += operator.textContent;
-    // displayNumber = result.textContent;
-    // console.log(displayNumber);
+
+        // if (e.target.innerText === '/') {
+        //     display.textContent += operator.textContent;
+        //     displayOperator === '/';
+        //     firstNum = displayNumber;
+        //     console.log(displayNumber);
+        // }
     }) 
 });
-
-
-
-
-
-
-// division.addEventListener('click', doDivide);
-
-// function doDivide() {
-//     if (displayNumber !== '' && !displayNumber.includes('/')) {
-//         firstNum = displayNumber;
-//         console.log('this is firstnum '+firstNum);
-//     } else if (displayNumber !== '' && displayNumber.includes('/')) {
-//         secondNum = displayNumber;
-//         console.log('this is secondnum '+secondNum);
-//     } else {
-//         console.log('error');
-//     }
-// }
-
