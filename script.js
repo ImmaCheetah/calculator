8f3d111678b226d74295edba82106a45546347cf
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2023-07-31 11:13:47 -0400

    Add more to numbers and operators
    - Add if else to numbers to decide which number to store
    - Add operate function to operator when = is clicked

--- script.js
+++ script.js
@@ -26,58 +26,66 @@ function multiply(...nums) {
     })
     return totalMult;
 }
-function operate(operator, firstNum, secondNum) {
-    if (operator === '+') {
-        return add(firstNum, secondNum);
-    } else if (operator === '-') {
-        return subtract(firstNum, secondNum);
-    } else if (operator === '/') {
-        return divide(firstNum, secondNum);
-    } else if (operator === '*') {
-        return multiply(firstNum, secondNum);
+function operate(num1, operatorDisplay, num2) {
+    if (operatorDisplay === '+') {
+        return add(num1, num2);
+    } else if (operatorDisplay === '-') {
+        return subtract(num1, num2);
+    } else if (operatorDisplay === '/') {
+        return divide(num1, num2);
+    } else if (operatorDisplay === '*') {
+        return multiply(num1, num2);
     } else {
         return 'Wha happen';
     }
 }
+
+console.log(operate(8, '/', 2));
 
 const numbers = document.querySelectorAll('.number-btn');
 const operators = document.querySelectorAll('.operator-btn');
 
 let display = document.querySelector('#result');
 const division = document.getElementById('divide');
-let displayOperator = '';
-let displayNumber = '';
+// let displayOperator = '';
+// let displayNumber = '';
 
 
 function updateDisplay() {
     let displayValue = num1 + operatorDisplay + num2;
     display.textContent = displayValue;
-    console.log(displayValue);
+    // console.log(displayValue);
 }
 
 
 // Add event listener to all number buttons to show content
 numbers.forEach(number => {
     number.addEventListener('click', function() {
-        num1 += number.textContent;
-        updateDisplay();
-        if (display.textContent.includes(num1, '/*-+')) {
+        if (operatorDisplay == '') {
+            num1 += number.textContent;
+          } else {
             num2 += number.textContent;
-        }
+          }
+          updateDisplay();
     }) 
 });
 
 operators.forEach(operator => {
     operator.addEventListener('click', e => {
-        operatorDisplay += operator.textContent;
-        console.log(operatorDisplay);
-        updateDisplay();
+        if (operator.textContent === '=') {
+            operate(num1, operatorDisplay, num2);  
+            updateDisplay(); 
+        } else {
+            operatorDisplay += operator.textContent;
+            // console.log(operatorDisplay);
+            updateDisplay();
+        }
 
 
         // if (e.target.innerText === '/') {
         //     display.textContent += operator.textContent;
         //     displayOperator === '/';
-        //     firstNum = displayNumber;
+        //     num1 = displayNumber;
         //     console.log(displayNumber);
         // }
     }) 
